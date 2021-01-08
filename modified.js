639816fb8bd4171eca6094783c4c9725385c8ba0
couchpotatodev <mjrimayaho@gmail.com>
2021-01-08 22:01:16 +0800

    added modified.js

---
+++ modified.js
@@ -0,0 +1,35 @@
+function lastModified(){
+  var modiDate=new Date(document.lastModified);
+  var showAs=modiDate.getDate()+ "-" +(modiDate.getMonth()+1)+ "-" +modiDate.getFullYear();
+
+  return showAs
+}
+
+function GetTime(){
+  var modiDate = new Date();
+  var Seconds
+
+    if (modiDate.getSeconds() < 10) {
+
+    Seconds = "0" + modiDate.getSeconds();
+
+  } else {
+
+    Seconds = modiDate.getSeconds();
+
+  }
+
+  var modiDate = new Date();
+  var CurTime =  modiDate.getHours() + ":" + modiDate.getMinutes() + ":" + Seconds
+
+  return CurTime
+
+}
+
+  document.write ("Last updated: ")
+  document.write (lastModified() + "  " + GetTime() );
+
+ // document.write (" [D M Y 24 Hour Clock]")
+
+document.write ("");
+
