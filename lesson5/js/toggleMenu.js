2b115f749af9e4495b6b22dd989d169b43fec2d2
couchpotatodev <mjrimayaho@gmail.com>
2021-02-01 19:26:30 +0800

    added banner if friday

--- lesson5/js/toggleMenu.js
+++ lesson5/js/toggleMenu.js
@@ -1,5 +1,4 @@
 function toggleMenu(){
    document.getElementsByClassName("menu")[0].classList.toggle("responsive");
- 
-  /* window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};*/
+    
 }
