c5aa73a37ae0bc6535b4655119f533db94526c99
couchpotatodev <mjrimayaho@gmail.com>
2021-03-05 19:41:51 +0800

    added images, wireframes, homepage

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -9,96 +9,93 @@ fetch(requestURL)
     const town = jsonObject['towns'];
     
 
-   
-    /*for (let i = 0; i < town.length; i++ ) { */
-        let card = document.createElement('section');
-        let fish = document.createElement('h2'); 
-        let preston = document.createElement('h2'); 
-        let soda = document.createElement('h2'); 
+    for (let i = 0; i < town.length; i++ ) { 
+      let card = document.createElement('section');
+      let fish = document.createElement('h2'); 
+      let preston = document.createElement('h2'); 
+      let soda = document.createElement('h2'); 
 
-        let fishmotto = document.createElement('p');
-        let prestonmotto = document.createElement('p');
-        let sodamotto = document.createElement('p');
+      let fishmotto = document.createElement('p');
+      let prestonmotto = document.createElement('p');
+      let sodamotto = document.createElement('p'); 
+
+      let yearfish = document.createElement('p');
+      let yearpreston = document.createElement('p');
+      let yearsoda = document.createElement('p');
       
-        let yearfish = document.createElement('p');
-        let yearpreston = document.createElement('p');
-        let yearsoda = document.createElement('p');
+      let fishpopulation = document.createElement('p');
+      let prestonpopulation = document.createElement('p');
+      let sodapopulation = document.createElement('p');
 
-        let fishpopulation = document.createElement('p');
-        let prestonpopulation = document.createElement('p');
-        let sodapopulation = document.createElement('p');
+      let fishrain = document.createElement('p');
+      let prestonrain= document.createElement('p');
+      let sodarain = document.createElement('p');
 
-        let fishrain = document.createElement('p');
-        let prestonrain= document.createElement('p');
-        let sodarain = document.createElement('p');
+      let fishimage = document.createElement('img');
+      let prestonimage = document.createElement('img');
+      let sodaimage = document.createElement('img');
 
-        let image1 = document.createElement('img');
-        let image2 = document.createElement('img');
-        let image3 = document.createElement('img');
-
-
-        fish.textContent = town[1].name;
-        preston.textContent = town[5].name;
-        soda.textContent = town[6].name;
-        
-        fishmotto.textContent = town[1].motto;
-        prestonmotto.textContent = town[5].motto;
-        sodamotto.textContent = town[6].motto;
-
-        yearfish.textContent = "Year founded:" + ' ' + town[1].yearFounded;
-        yearpreston.textContent = "Year founded:" + ' ' + town[5].yearFounded;
-        yearsoda.textContent = "Year founded:" + ' ' + town[6].yearFounded;
-
-        fishpopulation.textContent = "Population:" + ' ' + town[1].currentPopulation;
-        prestonpopulation.textContent = "Population:" + ' ' + town[5].currentPopulation;
-        sodapopulation.textContent = "Population:" + ' ' + town[6].currentPopulation; 
-      
+      if (town[i].name == "Fish Haven"){
+        fish.textContent = town[i].name;
+        fishmotto.textContent = town[i].motto;
+        yearfish.textContent = "Year founded:" + ' ' + town[i].yearFounded;
+        fishpopulation.textContent = "Population:" + ' ' + town[i].currentPopulation;
         fishrain.textContent = "Annual Rain Fall:" + ' ' + town[1].averageRainfall;
-        prestonrain.textContent = "Annual Rain Fall:" + ' ' + town[5].averageRainfall;
+        fishimage.setAttribute('src','../lesson9/images/fishhaven.jpg');
+        fishimage.setAttribute('alt', "fish haven image");
+      }
+
+
+      if (town[i].name == "Preston"){
+        preston.textContent = town[i].name;
+        prestonmotto.textContent = town[i].motto;
+        yearpreston.textContent = "Year founded:" + ' ' + town[i].yearFounded;
+        prestonpopulation.textContent = "Population:" + ' ' + town[i].currentPopulation;
+        prestonrain.textContent = "Annual Rain Fall:" + ' ' + town[i].averageRainfall;
+        prestonimage.setAttribute('src','../lesson9/images/preston.jpg');
+        prestonimage.setAttribute('alt', "preston image");
+      }
+
+      if (town[i].name == "Soda Springs"){
+        soda.textContent = town[i].name;
+        sodamotto.textContent = town[i].motto;
+        yearsoda.textContent = "Year founded:" + ' ' + town[i].yearFounded;
+        sodapopulation.textContent = "Population:" + ' ' + town[i].currentPopulation;
         sodarain.textContent = "Annual Rain Fall:" + ' ' + town[6].averageRainfall; 
-       
-        image1.setAttribute('src', town[1].photo);
-        image2.setAttribute('src', town[5].photo);
-        image3.setAttribute('src', town[6].photo);
-        image1.setAttribute('alt', "photo of fish haven");
-        image2.setAttribute('alt', "photo of preston");
-        image3.setAttribute('alt', "soda springs");
-        
+        sodaimage.setAttribute('src','../lesson9/images/sodasprings.jpg');
+        sodaimage.setAttribute('alt', "soda springs image");
+      }
 
-        card.appendChild(fish);
-        card.appendChild(fishmotto);
-        card.appendChild(yearfish);
-        card.appendChild(fishpopulation);
-        card.appendChild(fishrain);
-        card.appendChild(image1);
+      card.appendChild(fish);
+      card.appendChild(fishmotto);
+      card.appendChild(yearfish);
+      card.appendChild(fishpopulation);
+      card.appendChild(fishrain);
+      card.appendChild(fishimage);
 
-        card.appendChild(preston);
-        card.appendChild(prestonmotto); 
-        card.appendChild(yearpreston);
-        card.appendChild(prestonpopulation);
-        card.appendChild(prestonrain);
-        card.appendChild(image2);
+      card.appendChild(preston);
+      card.appendChild(prestonmotto);
+      card.appendChild(yearpreston);
+      card.appendChild(prestonpopulation);
+      card.appendChild(prestonrain);
+      card.appendChild(prestonimage);
 
-        card.appendChild(soda);
-        card.appendChild(sodamotto);
-        card.appendChild(yearsoda);
-        card.appendChild(sodapopulation);
-        card.appendChild(sodarain);
-        card.appendChild(image3);
-       
+      card.appendChild(soda);
+      card.appendChild(sodamotto);
+      card.appendChild(yearsoda);
+      card.appendChild(sodapopulation);
+      card.appendChild(sodarain);
+      card.appendChild(sodaimage);
 
 
-     
+      document.querySelector('div.cards').appendChild(card);
 
-
-      
+    }
 
 
 
 
 
-        document.querySelector('div.cards').appendChild(card);  
-    /*}    */
       
     
 
