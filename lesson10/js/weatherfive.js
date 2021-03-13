a2805588b363937be0dd9a9a81d99a3bda2173db
couchpotatodev <mjrimayaho@gmail.com>
2021-03-13 22:38:55 +0800

    updated js file

--- lesson10/js/weatherfive.js
+++ lesson10/js/weatherfive.js
@@ -6,12 +6,12 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
 
-   //current 
+   /*current 
    document.getElementById('humid').textContent = jsObject.list[0].main.humidity;
     document.getElementById('temp').textContent = jsObject.list[0].main.temp;
     document.getElementById('maxtemp').textContent = jsObject.list[0].main.temp_max;
     document.getElementById('windSpeed').textContent = jsObject.list[0].wind.speed;
-
+*/
 
 
   //5 day
