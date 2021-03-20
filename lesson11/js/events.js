75e0c77af475672506c21ad80ac1b72b2f0507d9
couchpotatodev <mjrimayaho@gmail.com>
2021-03-20 22:36:29 +0800

    added link in thanks

--- lesson11/js/events.js
+++ lesson11/js/events.js
@@ -8,17 +8,20 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  /* temporary checking for valid response and data parsing */
+    /*console.table(jsonObject);   temporary checking for valid response and data parsing */
     const town = jsonObject['towns'];
  
    let prestonevents = document.createElement('p');  
-   
+   let eventlist;
 
     for (let i = 0; i < town.length; i++ ) { 
       
        if (town[i].name == "Preston"){
+            eventlist= town[i].events;         
             prestonevents.textContent =  town[i].events;
             preston.appendChild(prestonevents);
+
+
        }
       
        
