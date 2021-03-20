d1bfb1137764c1366e6d620a8cd6a19eefeec1aa
couchpotatodev <mjrimayaho@gmail.com>
2021-03-20 16:51:54 +0800

    removed unnecessary comments

--- lesson11/js/events.js
+++ lesson11/js/events.js
@@ -1,19 +1,16 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 var preston = document.querySelector('.prestoncard');
-/*var soda = document.querySelector('.sodacard');
-var fish = document.querySelector('.fishcard');*/
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    /*console.table(jsonObject);   temporary checking for valid response and data parsing */
+    
     const town = jsonObject['towns'];
 
    let prestonevents = document.createElement('p');  
-   /*let sodaevents = document.createElement('p');  
-   let fishhavenevents = document.createElement('p');  */
+  
 
     for (let i = 0; i < town.length; i++ ) { 
         if (town[i].name == "Preston"){
@@ -21,18 +18,6 @@ fetch(requestURL)
             preston.appendChild(prestonevents);
           }
 
-/*
-       if (town[i].name == "Soda Springs"){
-            sodaevents.textContent =  town[i].events;
-            soda.appendChild(sodaevents);
-       }
-        
-       if (town[i].name == "Fish Haven"){
-        fishhavenevents.textContent = town[i].events;
-        fish.appendChild(fishhavenevents);
-      }
-    
-  */
        
     }   
 
--- lesson11/js/eventsfish.js
+++ lesson11/js/eventsfish.js
@@ -6,16 +6,13 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    /*console.table(jsonObject);   temporary checking for valid response and data parsing */
     const town = jsonObject['towns'];
 
 
    let fishhavenevents = document.createElement('p');  
 
     for (let i = 0; i < town.length; i++ ) { 
-       
-
-       
+             
        if (town[i].name == "Fish Haven"){
         fishhavenevents.textContent = town[i].events;
         fish.appendChild(fishhavenevents);
