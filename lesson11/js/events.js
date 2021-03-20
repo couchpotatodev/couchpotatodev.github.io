9903b78f93160b244465dadf4884933d8190b0cd
couchpotatodev <mjrimayaho@gmail.com>
2021-03-20 22:11:19 +0800

    updated map

--- lesson11/js/events.js
+++ lesson11/js/events.js
@@ -1,26 +1,31 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 var preston = document.querySelector('.prestoncard');
 
+
+
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    
+    console.table(jsonObject);  /* temporary checking for valid response and data parsing */
     const town = jsonObject['towns'];
-
+ 
    let prestonevents = document.createElement('p');  
-  
+   
 
     for (let i = 0; i < town.length; i++ ) { 
-        if (town[i].name == "Preston"){
-            prestonevents.textContent = town[i].events;
+      
+       if (town[i].name == "Preston"){
+            prestonevents.textContent =  town[i].events;
             preston.appendChild(prestonevents);
-          }
-
+       }
+      
        
     }   
 
 
 });
+
+
 
