b512e767e6893f52ce4cd58cccabc3d77a5e4a4e
couchpotatodev <mjrimayaho@gmail.com>
2021-03-21 11:44:13 +0800

    removed eventlist

--- lesson11/js/events.js
+++ lesson11/js/events.js
@@ -12,12 +12,12 @@ fetch(requestURL)
     const town = jsonObject['towns'];
  
    let prestonevents = document.createElement('p');  
-   let eventlist;
+  
 
     for (let i = 0; i < town.length; i++ ) { 
       
        if (town[i].name == "Preston"){
-            eventlist= town[i].events;         
+                
             prestonevents.textContent =  town[i].events;
             preston.appendChild(prestonevents);
 
