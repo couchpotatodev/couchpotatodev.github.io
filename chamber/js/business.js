14fc4f392fc44d78aba4c53a8e4cf77c4dfedd05
couchpotatodev <mjrimayaho@gmail.com>
2021-04-07 11:19:27 +0800

    edited json file

--- chamber/js/business.js
+++ chamber/js/business.js
@@ -11,17 +11,18 @@ var baguioBiz = document.querySelector('.cards');
 
 
 //const requestURL = 'https://github.com/couchpotatodev/couchpotatodev.github.io/blob/master/json/business.json';
-const requestURL = 'https://drive.google.com/file/d/1gzIn7sOMiNMN79VD-GL0DSTBc9Ax3cqm/view?usp=sharing';
+//const requestURL = 'https://drive.google.com/file/d/1gzIn7sOMiNMN79VD-GL0DSTBc9Ax3cqm/view?usp=sharing';
+const requestURL = '';
 var baguioBiz = document.querySelector('.cards');
 fetch(requestURL)
   .then(function (response) {
     return response.json();
-    mode: 'no-cors';
+  
   })
 
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const biz = jsonObject['prophets']; 
+    const biz = jsonObject['businesses']; 
 
 
 
