30f50d80e1ec682adf57d4979c60852670892ade
couchpotatodev <mjrimayaho@gmail.com>
2021-04-07 18:37:24 +0800

    edited js files

--- chamber/js/gridview.js
+++ chamber/js/gridview.js
@@ -1,6 +1,8 @@
 //document.getElementsByClassName("cards").style.display = "grid";
 //document.getElementbyClassName("cards").style.gridTemplateColumns = "repeat(auto-fill,minmax(300px,1fr));";
+/*
 function grid(){
 document.getElementsByClassName("gridcards2").style.display = "grid";
 document.getElementsByClassName("cards").style.display = "none";
 }
+*/
--- chamber/js/listview.js
+++ chamber/js/listview.js
@@ -2,7 +2,16 @@
 //var elements = document.getElementsByClassName("cards");
  
 //document.getElementsByClassName("cards").style.display = "block";
+/*
 function list(){
 document.getElementsByClassName("gridcards2").style.display = "none";
 document.getElementsByClassName("cards").style.display = "block";
 }
+*/
+/*
+
+function display_hidden_fields()
+{
+    alert("JS Function");
+    document.getElementById("hidden").style.display = "block";
+}*/
