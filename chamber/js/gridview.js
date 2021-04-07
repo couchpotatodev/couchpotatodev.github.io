1fa842706ce6ddfa1a4c81f763f5fdf9d01b40de
couchpotatodev <mjrimayaho@gmail.com>
2021-04-07 19:32:57 +0800

    edited js file

--- chamber/js/gridview.js
+++ chamber/js/gridview.js
@@ -10,7 +10,7 @@ document.getElementsByClassName("cards").style.display = "none";
 
 function grid(){
     document.getElementById("gridcards2").style.display = "grid";
-    document.getElementById("gridcards2").style.gridTemplateColumns = "repeat(auto-fill,minmax(300px,1fr));";
+    document.getElementById("gridcards2").style.gridTemplateColumns = "1fr 1fr";
     document.getElementById("gridcards2").style.gridGap = "10px";
     document.getElementById("listcards").style.display = "none";
     }
