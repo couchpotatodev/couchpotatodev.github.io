3f27c87f5a8fffd5e509fb3f138070cf65438490
couchpotatodev <mjrimayaho@gmail.com>
2021-04-07 18:09:06 +0800

    edited views

--- chamber/js/gridview.js
+++ chamber/js/gridview.js
@@ -1,6 +1,6 @@
 //document.getElementsByClassName("cards").style.display = "grid";
 //document.getElementbyClassName("cards").style.gridTemplateColumns = "repeat(auto-fill,minmax(300px,1fr));";
 function grid(){
-document.getElementsByClassName("cards2").style.display = "grid";
+document.getElementsByClassName("gridcards2").style.display = "grid";
 document.getElementsByClassName("cards").style.display = "none";
 }
--- chamber/js/listview.js
+++ chamber/js/listview.js
@@ -3,6 +3,6 @@
  
 //document.getElementsByClassName("cards").style.display = "block";
 function list(){
-document.getElementsByClassName("cards2").style.display = "none";
+document.getElementsByClassName("gridcards2").style.display = "none";
 document.getElementsByClassName("cards").style.display = "block";
 }
