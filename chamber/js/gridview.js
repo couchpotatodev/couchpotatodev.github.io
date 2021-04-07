32cc23a6865dd892c36d1e614f8f0e0767cc81d3
couchpotatodev <mjrimayaho@gmail.com>
2021-04-07 19:22:21 +0800

    edited js file

--- chamber/js/gridview.js
+++ chamber/js/gridview.js
@@ -10,6 +10,8 @@ document.getElementsByClassName("cards").style.display = "none";
 
 function grid(){
     document.getElementById("gridcards2").style.display = "grid";
+    document.getElementById("gridcards2").style.gridTemplateColumns = "repeat(auto-fill,minmax(300px,1fr));";
+    document.getElementById("myDIV").style.gridGap = "10px";
     document.getElementById("listcards").style.display = "none";
     }
     
