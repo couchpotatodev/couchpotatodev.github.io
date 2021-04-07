f9dd12407cf490c6ac0c2035fae449408c5a0fc7
couchpotatodev <mjrimayaho@gmail.com>
2021-04-07 17:09:35 +0800

    edited grid

--- chamber/js/business.js
+++ chamber/js/business.js
@@ -17,20 +17,32 @@ fetch(requestURL)
         let card2 = document.createElement('section');
         let h3 = document.createElement('h3'); 
         let contactinfo = document.createElement('p');
-        //let wlink = document.createElement('p');
         let clogo = document.createElement('img');
         let wlink = document.createElement('a');
         let next = document.createElement('br');
+   
+        let gridh3 = document.createElement('h3'); 
+        let gridcontactinfo = document.createElement('p');
+        let gridclogo = document.createElement('img');
+        let gridwlink = document.createElement('a');
+        let gridnext = document.createElement('br');
         
      
 
         h3.textContent = biz[i].name;
-        contactinfo.textContent = 'Contact Information:' + ' ' + biz[i].contact;
-      //  wlink.textContent = 'Website Link:' + ' ' + biz[i].weblink;     
+        contactinfo.textContent = 'Contact Information:' + ' ' + biz[i].contact; 
         clogo.setAttribute('src', biz[i].logo);
         clogo.setAttribute('alt', h3.textContent + 'logo');
         wlink.textContent = biz[i].weblink;
         wlink.href = biz[i].weblink;
+
+        gridh3.textContent = biz[i].name;
+        gridcontactinfo.textContent = 'Contact Information:' + ' ' + biz[i].contact; 
+        gridclogo.setAttribute('src', biz[i].logo);
+        gridclogo.setAttribute('alt', h3.textContent + 'logo');
+        gridwlink.textContent = biz[i].weblink;
+        gridwlink.href = biz[i].weblink;
+
 
 
         card.appendChild(h3);
@@ -40,11 +52,11 @@ fetch(requestURL)
         card.appendChild(wlink);
      
         
-        card2.appendChild(h3);
-        card2.appendChild(contactinfo);
-        card2.appendChild(clogo);
-        card2.appendChild(next); 
-        card2.appendChild(wlink);
+        card2.appendChild(gridh3);
+        card2.appendChild(gridcontactinfo);
+        card2.appendChild(gridclogo);
+        card2.appendChild(gridnext); 
+        card2.appendChild(gridwlink);
    
         
         baguioBiz.appendChild(card);  
