3d925e3988dc40a0b8243b537518ad08e015ca3f
couchpotatodev <mjrimayaho@gmail.com>
2021-04-07 17:39:27 +0800

    edited list and grid

--- chamber/js/listview.js
+++ chamber/js/listview.js
@@ -2,6 +2,7 @@
 //var elements = document.getElementsByClassName("cards");
  
 //document.getElementsByClassName("cards").style.display = "block";
-
+function list(){
 document.getElementsByClassName("cards2").style.display = "none";
 document.getElementsByClassName("cards").style.display = "block";
+}
