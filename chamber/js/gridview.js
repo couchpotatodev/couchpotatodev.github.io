5d08e83f932391425bf4719e053b5173edb6873f
couchpotatodev <mjrimayaho@gmail.com>
2021-04-07 17:44:39 +0800

    edited gridview

--- chamber/js/gridview.js
+++ chamber/js/gridview.js
@@ -1,6 +1,6 @@
 //document.getElementsByClassName("cards").style.display = "grid";
 //document.getElementbyClassName("cards").style.gridTemplateColumns = "repeat(auto-fill,minmax(300px,1fr));";
 function list(){
-document.getElementsByClassName("cards2").style.display = "block";
+document.getElementsByClassName("cards2").style.display = "grid";
 document.getElementsByClassName("cards").style.display = "none";
 }
